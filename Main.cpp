4b261a72b11148c9df06876450026dc3374783f0
Artem-Yusipov <46843927+Artem-Yusipov@users.noreply.github.com>
2020-06-25 10:33:55 +0300

    Task 2 added

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,39 @@
 #include <opencv2/opencv.hpp>
 #include <ViZDoom.h>
 
-double count = 0;
+
 std::string path = "C:\\practice\\vizdoom";
 auto game = std::make_unique<vizdoom::DoomGame>();
 const unsigned int sleepTime = 1000 / vizdoom::DEFAULT_TICRATE;
+auto episodes = 10;
+auto screenBuff = cv::Mat(480, 640, CV_8UC3);
+double count = 0;
 
-void runTask1(int episode)
+std::vector<double> killsprite(int x, int a)
+{
+    std::vector<double> actions[4];
+    actions[0] = { 1, 0, 0, 0 };
+    actions[1] = { 0, 1, 0, 0 };
+    actions[2] = { 0, 0, 1, 0 };
+    actions[3] = { 0, 0, 0, 1 };
+
+    if (x > 320 + a)
+    {
+        return actions[1];
+    }
+    if (x < 320 - a)
+    {
+        return actions[0];
+    }
+    else
+    {
+        return actions[3];
+    }
+}
+
+
+
+void RunTask1(int episode)
 {
 	try
 	{
@@ -104,23 +131,113 @@ void runTask1(int episode)
 
 
 
-int main()
+
+void RunTask2(int episode)
 {
 	try
 	{
-		game->setViZDoomPath(path + "\\vizdoom.exe");
-		game->setDoomGamePath(path + "\\freedoom2.wad");
+		game->loadConfig(path + "\\scenarios\\task2.cfg");
+		game->init();
 	}
 	catch (std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
 	}
 
-	auto episodes = 10;
+	std::vector<double> actions[4];
 
-	runTask1(episodes);
+	auto image = cv::Mat(480, 640, CV_8UC3);
+	auto greyscale = cv::Mat(480, 640, CV_8UC1);
 
-	std::cout << std::endl << "Avarage sum: " << count / episodes << std::endl;
+	cv::Mat clusters;
 
-	game->close();
+	actions[0] = { 1, 0, 0, 0 };
+	actions[1] = { 0, 1, 0, 0 };
+	actions[2] = { 0, 0, 1, 0 };
+	actions[3] = { 0, 0, 0, 1 };
+
+	for (auto i = 0; i < episode; i++)
+	{
+		game->newEpisode();
+		std::cout << "Episode #" << i + 1 << std::endl;
+
+		while (!game->isEpisodeFinished())
+		{
+			const auto& gameState = game->getState();
+			std::memcpy(image.data, gameState->screenBuffer->data(), gameState->screenBuffer->size());
+
+			std::vector<cv::Point2f> centers;
+			std::vector<cv::Point2f> points(0);
+			for (int x = 0; x < 640; x++)
+			{
+				for (int y = 0; y < 480; y++)
+				{
+					if (int(image.at<cv::Vec3b>(y, x)[2]) > 130 && int(image.at<cv::Vec3b>(y, x)[0]) < 50)
+					{
+						greyscale.at<unsigned char>(y, x) = 255;
+						points.push_back(cv::Point2f(x, y));
+					}
+					else
+					{
+						greyscale.at<unsigned char>(y, x) = 0;
+					}
+				}
+			}
+
+			greyscale.convertTo(greyscale, CV_32F);
+
+			cv::Mat samples = greyscale.reshape(1, greyscale.total());
+
+			cv::kmeans(points, 1, clusters, cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0), 3, cv::KMEANS_RANDOM_CENTERS, centers);
+
+			greyscale.convertTo(greyscale, CV_8UC3);
+
+			for (int i = 0; i < centers.size(); i++)
+			{
+				cv::Point c = centers[i];
+
+				cv::circle(image, c, 5, cv::Scalar(0, 0, 255), -1, 8);
+				cv::rectangle(image, cv::Rect(c.x - 25, c.y - 25, 50, 50), cv::Scalar(0, 0, 255));
+			}
+
+			for (int i = 0; i < points.size(); i++)
+			{
+				cv::circle(image, points[i], 2, cv::Scalar(0, 255, 0));
+			}
+
+			imshow("Grey", greyscale);
+			imshow("Claster", image);
+
+			if ((centers[0].x - 320) > 35)
+			{
+				game->makeAction(actions[1]);
+			}
+			else if ((centers[0].x - 320) < -35)
+			{
+				game->makeAction(actions[0]);
+			}
+			else
+			{
+				game->makeAction(actions[3]);
+			}
+
+			cv::waitKey(sleepTime);
+		}
+		std::cout << game->getTotalReward() << std::endl;
+		count += game->getTotalReward();
+	}
+}
+
+
+int main()
+{
+    game->setViZDoomPath(path + "\\vizdoom.exe");
+    game->setDoomGamePath(path + "\\freedoom2.wad");
+
+    RunTask2(episodes);
+
+    std::cout << " " << std::endl;
+    std::cout << "Average sum " << count / 10 << std::endl;
+
+    game->close();
 }
